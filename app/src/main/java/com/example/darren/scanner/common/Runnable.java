714a6c375a9b695715dc2b7bb4920885d47b4e5e
sunzhec2702 <sunzhe_c2702@163.com>
2015-10-20 23:22:41 +0800

    import a QR scanner
    Import the QR scanner.
    Next step will be modify the CaptureActivity to receive Intent and
    return the result.

---
+++ app/src/main/java/com/example/darren/scanner/common/Runnable.java
@@ -0,0 +1,21 @@
+package com.example.darren.scanner.common;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Build;
+
+/**
+ * 兼容低版本的子线程开启任务
+ * 
+ * @author hugo
+ * 
+ */
+
+public class Runnable {
+
+	@SuppressLint("NewApi")
+	@SuppressWarnings("unchecked")
+	public static void execAsync(AsyncTask<?, ?, ?> task) {
+			task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, null);
+	}
+}
