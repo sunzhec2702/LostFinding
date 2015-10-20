ee2c5da5c81104bae324a5e5b25fadc32ebf921c
sunzhec2702 <sunzhe_c2702@163.com>
2015-10-21 00:11:26 +0800

    start intent to scanner
    Add the permission for camera, flash and so on.
    start Intent by the button.
    Next step is to receive the string from the scanner.

--- app/src/main/java/com/example/darren/lostfinding/MainActivity.java
+++ app/src/main/java/com/example/darren/lostfinding/MainActivity.java
@@ -1,19 +1,35 @@
 package com.example.darren.lostfinding;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.darren.scanner.CaptureActivity;
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
+    private String decodeResult;
+    final String logTag = "LostFinding";
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(logTag, "in the onActivityResult");
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button scanButton = (Button) findViewById(R.id.scanner);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -24,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent scanIntent = new Intent(MainActivity.this, CaptureActivity.class);
+                startActivityForResult(scanIntent, 0);
+            }
+        });
+
+
     }
 
 }
