659852ea15d2319304735e41e4916d5db876f3af
sunzhec2702 <sunzhe_c2702@163.com>
2015-10-21 23:31:57 -0700

    Add a position service
    - Press + to get the position
    - fix the null pointer from CaptureActivity

--- app/src/main/java/com/example/darren/lostfinding/MainActivity.java
+++ app/src/main/java/com/example/darren/lostfinding/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.darren.lostfinding;
 
-import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -13,19 +13,36 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.darren.scanner.CaptureActivity;
-import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
     private String decodeResult;
     private Button scanButton;
     private TextView scanResult;
+    private TextView positionView;
     final String logTag = "LostFinding";
+    Handler posHandler = new Handler();
+    private PositionUpdate posUpdate;
+    private boolean isPositionStart = false;
 
+    Runnable posRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Location loc = posUpdate.getLoc();
+            if (loc != null) {
+                Log.d(logTag, "Not null");
+                positionView.setText(loc.getLatitude() + " , " + loc.getLongitude());
+            } else {
+                Log.d(logTag, "NULL");
+            }
+            posHandler.postDelayed(posRunnable, 1000);
+        }
+    };
+ stat
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.d(logTag, "in the onActivityResult");
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0) {
+        if (data != null && requestCode == 0) {
             String result = data.getStringExtra("result");
             if (result != null)
                 scanResult.setText(result);
@@ -37,16 +54,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         scanButton = (Button) findViewById(R.id.scanner);
         scanResult = (TextView) findViewById(R.id.scanResult);
-        setSupportActionBar(toolbar);
+        positionView = (TextView) findViewById(R.id.positionText);
+        posUpdate = new PositionUpdate(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (!isPositionStart) {
+                    positionView.setText("Started");
+                    isPositionStart = true;
+                    posHandler.post(posRunnable);
+                }
+                else {
+                    isPositionStart = false;
+                    posHandler.removeCallbacks(posRunnable);
+                    positionView.setText("stopped");
+                }
             }
         });
 
